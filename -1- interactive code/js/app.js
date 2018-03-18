c0f8011037c8e8c3382c8524f1c86d3db725821e
Maxim <max.k194@gmail.com>
2018-03-19 00:13:35 +0300

    second commit

---
+++ -1- interactive code/js/app.js
@@ -0,0 +1,20 @@
+function createTodoItem(title){
+	const checkbox = document.createElement('input');
+	// stop in 15:20
+}
+
+function addTodoItem(event){
+	event.preventDefault();
+
+	if (addInput.value === '') return alert('Необходимо ввести название задачи');
+
+	const listItem = createTodoItem(addInput.value);
+
+}
+
+const todoForm = document.getElementById('todo-form');
+const addInput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
+todoForm.addEventListener('submit', addTodoItem);
